c3a5129a90c8a53251e6321833baa8443ef49ba3
adriano-ms <amaciel_sanchez@hotmail.com>
2024-06-09 11:34:21 -0300

    Implementado alguns métodos

--- src/controller/ControladorCliente.java
+++ src/controller/ControladorCliente.java
@@ -1,31 +1,105 @@
 package controller;
 
-import model.Cliente;
+import model.bd.PessoaFisicaBD;
+import model.bd.PessoaJuridicaBD;
+import model.entitites.Cliente;
+import model.entitites.PessoaFisica;
+import model.entitites.PessoaJuridica;
 
 public class ControladorCliente {
 
+	private PessoaFisicaBD pessoaFisicaBD;
+	private PessoaJuridicaBD pessoaJuridicaBD;
+	
 	public ControladorCliente() {
 		super();
 	}
 
-	public void cadastrar(Cliente cliente) {
+	public void setPessoaFisicaBD(PessoaFisicaBD pessoaFisicaBD) {
+		this.pessoaFisicaBD = pessoaFisicaBD;
+	}
+
+	public void setPessoaJuridicaBD(PessoaJuridicaBD pessoaJuridicaBD) {
+		this.pessoaJuridicaBD = pessoaJuridicaBD;
+	}
+
+	public void cadastrar(Cliente cliente) throws Exception {
+		validarDados(cliente);
+		cliente.setId(cliente.hashCode());
+		if(cliente instanceof PessoaFisica) {
+			pessoaFisicaBD.adicionar((PessoaFisica)cliente);
+		}
+		if(cliente instanceof PessoaJuridica) {
+			pessoaJuridicaBD.adicionar((PessoaJuridica)cliente);;
+		}
+	}
+	
+	public void consultar(int id) {
 		
 	}
 	
-	public Cliente consultar(int id) throws Exception {
+	private Integer definirId(Cliente cliente) {
+		if(cliente instanceof PessoaFisica) {
+			var clientes = pessoaFisicaBD.consultar();
+			
+		}
+		if(cliente instanceof PessoaJuridica) {
+			pessoaJuridicaBD.adicionar((PessoaJuridica)cliente);;
+		}
 		return null;
 	}
 	
-	public Cliente editar(Cliente cliente) throws Exception {
-		return null;
+	private void validarDados(Cliente cliente) throws Exception {
+		if(cliente == null) {
+			throw new Exception("O cliente não pode ser nulo!");	
+		}
+		if(cliente.getNome() == null || cliente.getNome().isBlank()) {
+			throw new Exception("Nome inválido!");
+		}
+		var endereco = cliente.getEndereco();
+		if(validarCampo(endereco.getLogradouro())) {
+			throw new Exception("Logradouro inválido!");
+		}
+		if(validarCampo(endereco.getComplemento())) {
+			throw new Exception("Complemento inválido!");
+		}
+		if(validarCampo(endereco.getCep())) {
+			throw new Exception("CEP inválido!");
+		}
+		if(validarCampo(endereco.getLogradouro())) {
+			throw new Exception("Logradouro inválido!");
+		}
+		
+		if(cliente instanceof PessoaFisica) {
+			validarPessoaFisica((PessoaFisica)cliente);
+		} else if(cliente instanceof PessoaJuridica) {
+			validarPessoaJuridica((PessoaJuridica)cliente);
+		}
 	}
 	
-	public Cliente remover(int id) throws Exception {
-		return null;
+	private Boolean validarCampo(String s) {
+		return s == null || s.isBlank();
 	}
 	
-	@SuppressWarnings("unused")
-	private boolean validarDados(Cliente cliente) {
-		return false;
+	private void validarPessoaFisica(PessoaFisica cliente) throws Exception {
+		if(validarCampo(cliente.getCpf())) {
+			throw new Exception("CPF inválido!");
+		}
+		if(validarCampo(cliente.getCelular())) {
+			throw new Exception("Celular inválido!");
+		}
 	}
+		
+	private void validarPessoaJuridica(PessoaJuridica cliente) throws Exception {
+		if(validarCampo(cliente.getCnpj())) {
+			throw new Exception("CNPJ inválido!");
+		}
+		if(validarCampo(cliente.getTelefone())) {
+			throw new Exception("Telefone inválido!");
+		}
+		if(validarCampo(cliente.getEmail())) {
+			throw new Exception("E-mail inválido!");
+		}
+	}
+	
 }
--- src/controller/ControladorCompra.java
+++ src/controller/ControladorCompra.java
@@ -3,37 +3,37 @@ package controller;
 import model.Carrinho;
 
 public class ControladorCompra {
-	
+
 	public ControladorCompra() {
 		super();
 	}
-	
+
 	public void pesquisarProduto(String texto) {
-		
+
 	}
-	
+
 	public void adicionarProduto(int codigo, int qtde) {
-		
+
 	}
-	
+
 	public void removerProduto(int codigo, int qtde) {
-		
+
 	}
-	
+
 	public void checkout(Carrinho carrinho) {
-			
+
 	}
-	
+
 	public int verificarEstoque(int codigo) {
 		return 0;
 	}
-	
+
 	private void atualizarEstoque() {
-		
+
 	}
-	
+
 	private void registrarCompra() {
-		
+
 	}
-	
+
 }
